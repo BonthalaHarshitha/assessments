1732afa1df96056e3c938d6394b548c903330e86
BonthalaHarshitha <harshithareddy203@gmail.com>
2024-06-16 11:23:59 +0530

    Add files via upload

---
+++ Task4/JunitTask/src/main/java/snippet/Snippet.java
@@ -0,0 +1,14 @@
+package snippet;
+
+public class Snippet {
+	
+	public int product(int number1,int number2)
+	{
+		return number1*number2;
+	}
+	public int division(int number1,int number2)
+	{
+		return number1/number2;
+	}
+}
+
